4cabd3f76175e2e2440c3740f34210325c0b1c0b
Jason Cao <jasonmmd6@gmail.com>
2024-07-23 15:06:56 -0400

    All good

---
+++ capers/Dog.java
@@ -0,0 +1,78 @@
+package capers;
+
+import java.io.File;
+import java.io.Serializable;
+import static capers.Utils.*;
+
+/** Represents a dog that can be serialized.
+ * @author Zhuoyuan Cao
+*/
+public class Dog implements Serializable {
+
+
+    public static void main(String[] args) {
+        System.out.println(DOG_FOLDER);
+    }// TODO
+
+    /** Folder that dogs live in. */
+    static final File DOG_FOLDER = Utils.join(CapersRepository.CWD, ".capers" , "dogs");
+
+    /** Age of dog. */
+    private int age;
+    /** Breed of dog. */
+    private String breed;
+    /** Name of dog. */
+    private String name;
+
+    /**
+     * Creates a dog object with the specified parameters.
+     * @param name Name of dog
+     * @param breed Breed of dog
+     * @param age Age of dog
+     */
+    public Dog(String name, String breed, int age) {
+        this.age = age;
+        this.breed = breed;
+        this.name = name;
+    }
+
+    /**
+     * Reads in and deserializes a dog from a file with name NAME in DOG_FOLDER.
+     *
+     * @param name Name of dog to load
+     * @return Dog read from file
+     */
+    public static Dog fromFile(String name) {
+        // TODO (hint: look at the Utils file)
+        File dogToRead = Utils.join(DOG_FOLDER, name);
+
+
+        return Utils.readObject(dogToRead, Dog.class);
+    }
+
+    /**
+     * Increases a dog's age and celebrates!
+     */
+    public void haveBirthday() {
+        age += 1;
+        System.out.println(toString());
+        System.out.println("Happy birthday! Woof! Woof!");
+    }
+
+    /**
+     * Saves a dog to a file for future use.
+     */
+    public void saveDog() {
+        // TODO (hint: don't forget dog names are unique)
+        File dogToSave = Utils.join(DOG_FOLDER, name);
+        Utils.writeObject(dogToSave, this);
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+            "Woof! My name is %s and I am a %s! I am %d years old! Woof!",
+            name, breed, age);
+    }
+
+}
